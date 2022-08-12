4088e9f563f46bfdeb13f0a4735fb82c85cfa5e9
thomas <tom@tomdoesdev.com>
2022-08-12 17:50:06 +0200

    Correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBEGaZHpQBolBfIov_JHC1I0DuPWgFIfOI',
-  authDomain: 'koyohair-7040f.firebaseapp.com',
-  projectId: 'koyohair-7040f',
-  storageBucket: 'koyohair-7040f.appspot.com',
-  messagingSenderId: '978092193256',
-  appId: '1:978092193256:web:abd50474493279dc0c205d',
-};
 
 @Component({
   selector: 'app-root',
@@ -16,7 +6,5 @@ const firebaseConfig = {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  public ngOnInit(): void {
-    const app = initializeApp(firebaseConfig);
-  }
+  public ngOnInit(): void {}
 }
